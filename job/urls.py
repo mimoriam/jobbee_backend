d435786399174f5e4b3e999abcc94a454ec76472
Mimoriam <mimoriam420@gmail.com>
2022-04-10 20:54:15 +0500

    Commit #8 - GET, POST, PUT, DELETE and Custom endpoint

--- job/urls.py
+++ job/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('jobs/', views.get_all_jobs, name='jobs'),
-    path('jobs/<str:pk>', views.get_job, name='job')
+    path('jobs/<str:pk>', views.get_job, name='job'),
+    path('jobs/new/', views.post_job, name='new_job'),
+    path('jobs/<str:pk>/delete', views.delete_job, name='delete_job'),
+    path('jobs/<str:pk>/update', views.update_job, name='update_job'),
+    path('stats/<str:topic>/', views.get_topic_stats, name='get_topic_stats'),
 ]
