2aaedbb0e612bc5db965bd5888bbc574a08daad5
Mimoriam <mimoriam420@gmail.com>
2022-04-10 20:41:52 +0500

    Commit #7 - Get single job endpoint

--- job/urls.py
+++ job/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jobs/', views.get_all_jobs, name='jobs')
+    path('jobs/', views.get_all_jobs, name='jobs'),
+    path('jobs/<str:pk>', views.get_job, name='job')
 ]
--- job/views.py
+++ job/views.py
@@ -5,11 +5,22 @@ from .models import Job
 
 from .serializers import JobSerializer
 
+from django.shortcuts import get_object_or_404
+
 
 @api_view(['GET'])
 def get_all_jobs(request):
     jobs = Job.objects.all()
 
     serializer = JobSerializer(jobs, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_job(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    serializer = JobSerializer(job, many=False)
 
     return Response(serializer.data)
